dbbcd8fc255e57ad53094937d8ed06333ebf0b92
nanorisrael <israel.nanor@compozelabs.com>
2023-08-11 15:23:18 +0000

    Add sample catalog images

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,10 +4,10 @@ export default function Product({name, price, quantity, category, photo}: Produc
 
     return (
         <figure>
-            <img src={`./${photo}`} alt="" width={100} height={100}/>
+            <img src={`./images/${photo}`} alt="" width={100} height={100}/>
                 <figcaption>
-                    <p><strong>item:</strong>{name}</p>
-                    <p><strong>price:</strong>{price} </p>
+                    <p><strong>item:</strong><span>{name}</span></p>
+                    <p><strong>price:</strong><span><strong>GH¢</strong>{price.toFixed(2)}</span> </p>
                     {/* <p><strong>quantity:</strong>{quantity}</p> */}
                     {/* <p><strong>category:</strong>{category}</p> */}
                 </figcaption>
